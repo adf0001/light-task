0160a38df735447dedfd500bdea0e80febbeabad
fwg <adf0001@163.com>
2022-09-24 21:30:25 +0800

    build http server frame work

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	server_debug: 1,
+
+	http_host: '127.0.0.1',
+	http_port: 8071,
+
+	database: './db/light-task.sqlite',
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+﻿
+"use strict";
+
+console.log("============= system start =============");
+console.log(new Date());
+
+var http = require("http");
+//var https = require("https");
+
+var fs = require('fs');
+
+var express = require('express');
+//var bodyParser = require('body-parser');
+//var compression = require('compression');
+
+//var process = require("./process.js");
+var config = require('./config.js');
+
+var app = express();
+
+//app.use(compression());
+
+//var urlencodedParser = bodyParser.urlencoded({ extended: false, limit: '500kb', parameterLimit: 100000 });
+
+//http
+var httpServer;
+
+if (config.http_port >= 0) {
+	httpServer = http.createServer(app);
+	
+	httpServer.listen(config.http_port||80, config.http_host||"127.0.0.1", () => {
+		var addr= httpServer.address();
+		console.log("server run in HTTP mode at " + addr.address +":" + addr.port);
+	});
+}
+
