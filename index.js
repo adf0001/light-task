a0e44e219bbdfcb6827ec5dd7260345948e44457
fwg <adf0001@163.com>
2022-10-07 16:38:10 +0800

    split out module morgan-ym-d-log-stream

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 ﻿
-"use strict";
 
-console.log("============= system start =============");
-console.log((new Date()).toLocaleString());
+var path = require('path');
+var morgan_ym_d_log_stream = require("morgan-ym-d-log-stream")
+var logStream = morgan_ym_d_log_stream(path.join(__dirname, "log"));
+
+logStream.write("\n=============\nstart at " + (new Date()).toLocaleString() + "\n");
 
 var http = require("http");
 //var https = require("https");
-
-var fs = require('fs');
-var path = require('path');
 
 var express = require('express');
 var compression = require('compression');
@@ -18,7 +17,6 @@ var config = require('./config.js');
 var db = require('./lib/sqlite-db.js');
 var morgan_res_body = require('./lib/morgan-res-body.js');
 
-var morgan_ym_d_log_stream = require("./lib/morgan-ym-d-log-stream.js")
 
 var app = express();
 
@@ -34,12 +32,11 @@ morgan.token("req-body", (req, res) => {
 });
 morgan_res_body.addToken();
 
+
 var logger = morgan(
 	':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ' +
 	':req-body :res-body',
-	{
-		stream: morgan_ym_d_log_stream(path.join(__dirname, "log")),
-	}
+	{ stream: logStream, }
 );
 
 app.use(logger);
